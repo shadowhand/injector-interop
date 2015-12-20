398e86475879867dadc3df2d9fdee629fa1ace0b
Woody Gilk <woody.gilk@gmail.com>
2015-12-20 12:38:44 -0600

    Refine the injector interface
    Clarify that the invokable can be a class or class with method.
    
    Remove the ability to pass constructor arguments to `make`, which would
    be ignored if the class or alias had been shared.

--- src/InjectorInterface.php
+++ src/InjectorInterface.php
@@ -5,7 +5,7 @@ namespace Interop\Injector;
 interface InjectorInterface
 {
     /**
-     * Define instantiation directives for a class or alias
+     * Define constructor arguments for a class or alias
      *
      * @param string $spec
      * @param array $args
@@ -15,34 +15,40 @@ interface InjectorInterface
     public function define($spec, array $args);
 
     /**
-     * Register a callable to prepare an object instance
+     * Register a invokable to prepare an object instance
      *
-     * The callable must take two arguments: the instantiated object and the
+     * The invokable may be a PHP callable, the name of a class that has an
+     * `__invoke` method, or the name of a class with a non-static method.
+     *
+     * The invokable must take two arguments: the instantiated object and the
      * current injector.
      *
-     * If the callable returns an object of the same type, it will replace the
+     * If the invokable returns an object of the same type, it will replace the
      * current instance, to allow immutable objects to be mutated.
      *
      * @param string $spec
-     * @param callable $using
+     * @param callable|string $invokable
      *
      * @return self
      */
-    public function prepare($spec, callable $using);
+    public function prepare($spec, $invokable);
 
     /**
-     * Register a callable to create an object instance
+     * Register a invokable to create an object instance
      *
-     * The callable must take one argument: the injector.
+     * The invokable may be a PHP callable, the name of a class that has an
+     * `__invoke` method, or the name of a class with a non-static method.
      *
-     * The callable must return an instance of the specified class.
+     * The invokable must take one argument: the injector.
+     *
+     * The invokable must return an instance of the specified class.
      *
      * @param string $spec
-     * @param callable $using
+     * @param callable|string $invokable
      *
      * @return self
      */
-    public function delegate($spec, callable $using);
+    public function delegate($spec, $invokable);
 
     /**
      * Define an alias for a class
@@ -58,11 +64,10 @@ interface InjectorInterface
      * Create a new instance of a class and provision it
      *
      * @param string $spec
-     * @param array $args
      *
      * @return object
      */
-    public function make($spec, array $args = []);
+    public function make($spec);
 
     /**
      * Share the specified class/instance within the injector context
